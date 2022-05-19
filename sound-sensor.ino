6a392d19d8684f32764011cc2d1949cd299530bb
Jaedson Silva <contacanal798@gmail.com>
2022-05-18 21:51:35 -0300

    adding led control based in sound levels

--- sound-sensor.ino
+++ sound-sensor.ino
@@ -8,6 +8,13 @@ const int twoYellowPin = 13;
 const int oneRedPin = 11;
 const int twoRedPin = 12;
 
+// sound levels
+const int low = 120;
+const int medium = 170;
+const int high = 220;
+
+const int levelDiff = 60;
+
 void setup() {
   // pinMode of leds
   for(int i = 8; i < 13; i++) {
@@ -18,5 +25,46 @@ void setup() {
 }
 
 void loop() {
+  const int sound = analogRead(soundSensorPin);
 
+  // low sound
+  if(sound >= low - levelDiff) {
+    digitalWrite(oneBluePin, HIGH);
+  } else {
+    digitalWrite(oneBluePin, LOW);
+  }
+
+  if(sound >= low) {
+    digitalWrite(twoBluePin, HIGH);
+  } else {
+    digitalWrite(twoBluePin, LOW);
+  }
+
+  // medium sound
+  if(sound >= medium - levelDiff) {
+    digitalWrite(oneYellowPin, HIGH);
+  } else {
+    digitalWrite(oneYellowPin, LOW);
+  }
+
+  if(sound >= medium) {
+    digitalWrite(twoYellowPin, HIGH);
+  } else {
+    digitalWrite(twoYellowPin, LOW);
+  }
+
+  // high sound
+  if(sound >= high - levelDiff) {
+    digitalWrite(oneRedPin, HIGH);
+  } else {
+    digitalWrite(oneRedPin, LOW);
+  }
+
+  if(sound >= high) {
+    digitalWrite(twoRedPin, HIGH);
+  } else {
+    digitalWrite(twoRedPin, LOW);
+  }
+
+  delay(50);
 }
