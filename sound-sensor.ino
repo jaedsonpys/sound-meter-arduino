369ae77808b69102d6b3145a0ad3d034f947995a
Jaedson Silva <contacanal798@gmail.com>
2022-05-18 21:26:42 -0300

    adding pins of leds and pin sensor

---
+++ sound-sensor.ino
@@ -0,0 +1,19 @@
+const int soundSensorPin = A0;
+
+// leds pin
+const int oneBluePin = 8;
+const int twoBluePin = 9;
+const int oneYellowPin = 10;
+const int twoYellowPin = 13;
+const int oneRedPin = 11;
+const int twoRedPin = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
